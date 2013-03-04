85b27a79365cbb97f4ea6708ed25e1df8e8bc3c9
Johan Schuyt <johan@300.nl>
2013-03-04 22:18:57 +0100

    Use -n instead of -h for compatibility between BSD and Linux implementations

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 DIR_BASE=$( cd $(dirname $0) ; pwd )
 DIR_SKELETON=$DIR_BASE/homedir
 
-ls -A $DIR_SKELETON | xargs -n 1 -I R ln -hfs $DIR_SKELETON/R ~/R
+ls -A $DIR_SKELETON | xargs -n 1 -I R ln -nfs $DIR_SKELETON/R ~/R
