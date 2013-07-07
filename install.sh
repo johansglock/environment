245458fd7044b5feaec1cab4da7ce4f83a0c48d7
name <email>
2013-07-07 15:57:25 +0200

    Automatically ask and supply email + name for git configuration

--- install.sh
+++ install.sh
@@ -3,4 +3,26 @@
 DIR_BASE=$( cd $(dirname $0) ; pwd )
 DIR_SKELETON=$DIR_BASE/homedir
 
+echo "Linking configuration files..."
 ls -A $DIR_SKELETON | xargs -n 1 -I R ln -nfs $DIR_SKELETON/R ~/R
+echo "DONE"
+echo
+
+echo "Checking for unchanged Git options..."
+EMAIL_MATCHES=$(grep -c '<email>' ~/.gitconfig)
+if [ $EMAIL_MATCHES -gt 0 ]
+then
+    echo "Email: \c"
+    read EMAIL
+    sed -i '' -e "s/<email>/$EMAIL/g" $DIR_SKELETON/.gitconfig
+fi
+
+NAME_MATCHES=$(grep -c '<name>' ~/.gitconfig)
+if [ $NAME_MATCHES -gt 0 ]
+then
+    echo "Name: \c"
+    read NAME
+    sed -i '' -e "s/<name>/$NAME/g" $DIR_SKELETON/.gitconfig
+fi
+echo "DONE"
+echo
